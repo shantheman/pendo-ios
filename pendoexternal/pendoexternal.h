6b9213047f9caf962b1a6f28b78fb317ae1d08ba
Antonio Rodriguez <antonio@pendo.io>
2016-04-26 10:42:50 -0400

    Adding facade project.

---
+++ pendoexternal/pendoexternal.h
@@ -0,0 +1,19 @@
+//
+//  pendoexternal.h
+//  pendoexternal
+//
+//  Created by Antonio Rodriguez on 4/26/16.
+//  Copyright © 2016 Pendo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for pendoexternal.
+FOUNDATION_EXPORT double pendoexternalVersionNumber;
+
+//! Project version string for pendoexternal.
+FOUNDATION_EXPORT const unsigned char pendoexternalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pendoexternal/PublicHeader.h>
+
+
